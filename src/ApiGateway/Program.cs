72ba24072c47f4af931043f9aacade402f8b4795
Nahaq <naha022677@icloud.com>
2024-06-23 21:48:38 +0900

    ocelot作成

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -1,17 +1,37 @@
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables();
+builder.Services
+    .AddAuthentication()
+    .AddJwtBearer(option =>
+    {
+        option.TokenValidationParameters =
+        new TokenValidationParameters
+        {
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("Jwt:Jwt_Key") ?? string.Empty)
+            ),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
 builder.Services.AddOcelot(builder.Configuration);
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
 var app = builder.Build();
-await app.UseOcelot();
 
 app.UseAuthorization();
 app.UseAuthentication();
 
-app.Run();
+await app.UseOcelot();
+await app.RunAsync();
